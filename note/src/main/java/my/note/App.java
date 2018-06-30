f3acfa420672b27920420eae2eeac400455921ee
my <my@my.com>
2018-06-30 18:26:20 +0800

    init
    Signed-off-by: hai <hai@hai.com>

--- note/src/main/java/my/note/App.java
+++ note/src/main/java/my/note/App.java
@@ -4,10 +4,8 @@ package my.note;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ){
         System.out.println( "Hello World!" );
     }
 }
